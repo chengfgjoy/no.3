685c4d105d9bdd2d23dc7920b49c2861af4941c8
joy <chengfg@bc.edu>
2020-01-15 23:10:07 -0500

    Initial Commit

---
+++ no.3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  no.3
+//
+//  Created by cheng jiayi on 2020/1/15.
+//  Copyright © 2020 cheng jiayi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
