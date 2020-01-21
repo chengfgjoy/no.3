fc15c777190cab026f5c8b136bf2c9f0ba3eb49f
joy <chengfg@bc.edu>
2020-01-20 22:00:32 -0500

    2.8

--- no.3/ViewController.swift
+++ no.3/ViewController.swift
@@ -13,28 +13,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var showMessageLabel: UILabel!
     @IBOutlet weak var ImageView: UIImageView!
     
+    var ImageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func showMessageButton(_ sender: UIButton) {
-    let message1 = "You are awesome!"
-    let message2 = "You are great!"
-    let message3 = "You da bomb"
-  
-  
-        if showMessageLabel.text == message1{
-            showMessageLabel.text = message2
-             ImageView.image = UIImage(named:"WechatIMG167")
-        }else if showMessageLabel.text == message2{
-            showMessageLabel.text = message3
-            ImageView.image = UIImage (named:"WechatIMG166")
-        } else {
-            showMessageLabel.text  = message1
-            ImageView.image = UIImage (named:"WechatIMG273")
-            
+        print(ImageNumber)
+        //let ImageName = "Image" + String(ImageNumber)
+        let ImageName = "image\(ImageNumber)"
+        ImageView.image = UIImage(named: ImageName)
+        ImageNumber = ImageNumber + 1
+        if ImageNumber == 4 {
+            ImageNumber = 0
         }
+            
+//    let message1 = "You are awesome!"
+//    let message2 = "You are great!"
+//    let message3 = "You da bomb"
+//
+//
+//        if showMessageLabel.text == message1{
+//            showMessageLabel.text = message2
+//             ImageView.image = UIImage(named:"image1")
+//        }else if showMessageLabel.text == message2{
+//            showMessageLabel.text = message3
+//            ImageView.image = UIImage (named:"image2")
+//        } else {
+//            showMessageLabel.text  = message1
+//            ImageView.image = UIImage (named:"image3")
+//            
+//        }
         
 }
 }
